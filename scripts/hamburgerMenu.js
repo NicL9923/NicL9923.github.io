7a0aa8a9222c99f70a9cf5050021887d17b8217a
Nic9923 <nicl9923@gmail.com>
2020-07-26 20:49:22 -0500

    Reworking website/styling for the actual last time. Think I finally got a hold of modern design so that's what I'm going for.

---
+++ scripts/hamburgerMenu.js
@@ -0,0 +1,8 @@
+function showNavMenu() {
+    let x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
