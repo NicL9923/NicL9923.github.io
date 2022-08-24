701b3fa4efe7122072aa6bc11d6775b474ac600d
Nicolas Layne <nicl9923@gmail.com>
2022-08-24 18:05:25 -0500

    Add some content

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,16 +1,27 @@
+import { Body1, Divider, Image, Subtitle1, Title1, Title3 } from '@fluentui/react-components';
+
 const AboutMe = () => {
   return (
     <div>
-      <div>Nicolas Layne</div>
-      <div>Software Engineer</div>
-
       <div>
-        Hey there, nice to meet ya - I&apos;m the friendly neighborhood front-end-specializing software developer! I
-        love building and learning about things with a team of equally curious folks - it&apos;s really that simple!
+        <Image src='/Nic&Kim.jpg' alt='Nic & Kim' shape='circular' height={250} width={250} />
+
+        <Divider />
+
+        <Title1 block>Nicolas Layne</Title1>
+        <Subtitle1 block>Software Engineer</Subtitle1>
       </div>
 
       <div>
-        <div>Stacks & Skills</div>
+        <Body1>
+          Hey there, nice to meet ya - I&apos;m the friendly neighborhood front-end-specializing software developer! I
+          love building and learning about things with a team of equally curious folks - it&apos;s really that simple!
+        </Body1>
+      </div>
+
+      <div>
+        <Title3>Stacks & Skills</Title3>
+
         <div>
           <div>React</div>
           <div>TypeScript / JavaScript</div>
