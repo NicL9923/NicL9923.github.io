5850e3670f43a6de1dbd5082c5ce548f1cfd1452
Nicolas Layne <nicl9923@gmail.com>
2022-10-02 17:54:55 -0500

    Implement Chakra theme extension for dark mode support

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, type ThemeConfig } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+const chakraThemeConfig: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ chakraThemeConfig });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
