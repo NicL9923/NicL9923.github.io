a31d58781ebacddf7c758b7c2c9f9fd19c37dacb
Nicolas Layne <nicl9923@gmail.com>
2024-06-30 20:14:52 -0500

    Slim down content to keep single sources of truth (mostly in favor of LinkedIn)

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,14 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-  Container,
-  Stack,
-} from '@chakra-ui/react';
+import { Stack } from '@chakra-ui/react';
 
 import AboutMe from './components/AboutMe';
 import LightDarkModeToggle from './components/controls/LightDarkModeToggle';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
 
 const App = () => {
   return (
     <Stack direction='column' spacing={12} align='center'>
       <LightDarkModeToggle />
-      <div>
-        Yeah yeah, I know - I&apos;m working on it, but don&apos;t worry, the stuff I&apos;m getting paid to build
-        full-time looks a lot better!
-      </div>
 
       <AboutMe />
-
-      <Container maxW='container.lg' centerContent mt='16px'>
-        <Accordion defaultIndex={[0]} allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as='span' flex='1' textAlign='left'>
-                  Experience
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              <Experience />
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as='span' flex='1' textAlign='left'>
-                  Education
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              <Education />
-            </AccordionPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box as='span' flex='1' textAlign='left'>
-                  Projects
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              <Projects />
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Container>
     </Stack>
   );
 };
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,67 +1,68 @@
 import {
-  Image,
-  Divider,
+  Card,
+  CardBody,
   Container,
   HStack,
-  VStack,
   Heading,
-  Text,
   IconButton,
+  Image,
   Link,
+  ScaleFade,
+  Stack,
   Tag,
-  TagLeftIcon,
   TagLabel,
+  TagLeftIcon,
+  Text,
+  VStack,
 } from '@chakra-ui/react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { SiReact, SiTypescript, SiCsharp } from 'react-icons/si';
+import { SiReact, SiTypescript } from 'react-icons/si';
 
 const AboutMe = () => {
   return (
     <Container maxW='container.md' centerContent>
-      <HStack>
-        <Image borderRadius='full' boxSize='230px' src='/Nic&Kim.jpg' alt='Nic & Kim' />
+      <ScaleFade initialScale={0.5} transition={{ enter: { duration: 0.5 } }} in>
+        <Card>
+          <CardBody>
+            <Stack direction='column' spacing={4}>
+              <Stack direction={['column', 'row']} align='center' justifyContent='space-evenly'>
+                <Image borderRadius='full' boxSize='230px' src='/Nic&Kim.jpg' alt='Nic & Kim' />
 
-        <Divider orientation='vertical' height='175px' />
+                <VStack>
+                  <Heading size='xl'>Nicolas Layne</Heading>
+                  <Heading size='md'>Software Engineer</Heading>
 
-        <VStack>
-          <Heading>Nicolas Layne</Heading>
-          <Heading size='md'>Software Engineer</Heading>
-          <HStack>
-            <Link href='https://github.com/NicL9923' isExternal>
-              <IconButton icon={<FaGithub />} aria-label='Nicolas Github' />
-            </Link>
-            <Link href='https://linkedin.com/in/nicolas-layne/' isExternal>
-              <IconButton colorScheme='linkedin' icon={<FaLinkedin />} aria-label='Nicolas LinkedIn' />
-            </Link>
-          </HStack>
-        </VStack>
-      </HStack>
+                  <HStack mt={2}>
+                    <Link href='https://github.com/NicL9923' isExternal>
+                      <IconButton icon={<FaGithub />} aria-label="Nicolas' Github" />
+                    </Link>
+                    <Link href='https://linkedin.com/in/nicolas-layne/' isExternal>
+                      <IconButton colorScheme='linkedin' icon={<FaLinkedin />} aria-label="Nicolas' LinkedIn" />
+                    </Link>
+                  </HStack>
+                </VStack>
+              </Stack>
 
-      <div style={{ marginTop: 4 }}>
-        <Text>
-          Hey there, nice to meet ya - I&apos;m the friendly neighborhood front-end-specializing software developer! I
-          love building and learning about things with a team of equally curious folks - it&apos;s really that simple!
-        </Text>
-      </div>
+              <Text textAlign='center'>
+                Hey there, nice to meet ya - I&apos;m the friendly neighborhood front-end-specializing software
+                developer 🤠! I love building and learning about things with a team of equally curious and passionate
+                folks - it&apos;s really that simple!
+              </Text>
 
-      <div style={{ marginTop: 10 }}>
-        <Heading size='md'>Primary technical skills</Heading>
-
-        <HStack>
-          <Tag size='lg' colorScheme='cyan'>
-            <TagLeftIcon as={SiReact} />
-            <TagLabel>React</TagLabel>
-          </Tag>
-          <Tag size='lg' colorScheme='blue'>
-            <TagLeftIcon as={SiTypescript} />
-            <TagLabel>TypeScript</TagLabel>
-          </Tag>
-          <Tag size='lg' colorScheme='purple'>
-            <TagLeftIcon as={SiCsharp} />
-            <TagLabel>Some C#</TagLabel>
-          </Tag>
-        </HStack>
-      </div>
+              <HStack justifyContent='center'>
+                <Tag size='lg' colorScheme='cyan'>
+                  <TagLeftIcon as={SiReact} />
+                  <TagLabel>React</TagLabel>
+                </Tag>
+                <Tag size='lg' colorScheme='blue'>
+                  <TagLeftIcon as={SiTypescript} />
+                  <TagLabel>TypeScript</TagLabel>
+                </Tag>
+              </HStack>
+            </Stack>
+          </CardBody>
+        </Card>
+      </ScaleFade>
     </Container>
   );
 };
