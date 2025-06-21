ddbe8985f7a4cbfae5bd59fdd5280c8060bb88cb
Nicolas Layne <nicl9923@gmail.com>
2025-06-20 21:07:31 -0500

    Fix Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NicL9923.github.io/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
